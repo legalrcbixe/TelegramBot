066d59f491304b2bf7ba5d3f27a9d42c52e77268
Gronner <felix.braeunling@t-online.de>
2016-06-26 21:25:37 +0200

    Changed tdt to use requests instead of urllib to reduce dependencies

--- ComTdT.py
+++ ComTdT.py
@@ -4,7 +4,7 @@
 # Manages the tdt command from the bot
 
 from bs4 import BeautifulSoup
-import urllib.request
+import requests
 
 
 def get_tdt_img_url():
@@ -12,8 +12,8 @@ def get_tdt_img_url():
     Finds the image url of the newest TdT picture
     :return: string - url of the image
     """
-    tdt_url = urllib.request.urlopen('http://tittendestages.tumblr.com/')
-    tdt_soup = BeautifulSoup(tdt_url, "html.parser")
+    tdt_url = requests.get('http://tittendestages.tumblr.com/')
+    tdt_soup = BeautifulSoup(tdt_url.text, "html.parser")
     img_soup = tdt_soup.find("divabc", class_="post photo").a.img.attrs['src']
     img_url = str(img_soup)
     return img_url
